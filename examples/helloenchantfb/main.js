d0b3852283cc56ed477aa6534a8e8f10542fdca1
itog <itog@pigmal.com>
2013-06-16 16:02:33 +0900

    Add label on helloenchantfb

--- examples/helloenchantfb/main.js
+++ examples/helloenchantfb/main.js
@@ -4,22 +4,27 @@ var SCREEN_WIDTH = 640;
 var SCREEN_HEIGHT = 480;
 
 window.onload = function() {
-	var game = new Core(SCREEN_WIDTH, SCREEN_HEIGHT);
+    var game = new Core(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    var label = new Label("test");
+    label.x = 100;
+    label.y = 200;
+    game.rootScene.addChild(label)
 
     FourBeat.enableFourBeatOnScene(game.rootScene, function(event, color) {
         if (event == FourBeat.FB_EVENT_PRESS) {
             switch (color) {
             case FourBeat.FB_COLOR_RED:
-                console.log('red');
+		label.text = 'red';
                 break;
             case FourBeat.FB_COLOR_BLUE:
-                console.log('blue');
+		label.text = 'blue';
                 break
             case FourBeat.FB_COLOR_YELLOW:
-                console.log('yellow');
+		label.text = 'yellow';
                 break
             case FourBeat.FB_COLOR_GREEN:
-                console.log('green');
+		label.text = 'green';
                 break;
             default:
                 break;
